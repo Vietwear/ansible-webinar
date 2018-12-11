db2f39dd64ccaa3a63e0144ba216882cfe9274f9
Juergen Etzlstorfer <7677092+jetzlstorfer@users.noreply.github.com>
2018-12-11 11:19:17 +0100

    usage information added

--- add-to-cart.sh
+++ add-to-cart.sh
@@ -2,6 +2,14 @@
 echo "Usage: ./add-to-cart.sh http://XX.XX.XX.XX/carts/1/items"
 echo "Press [CTRL+C] to stop.."
 
+if [ -z $1 ]
+then
+    echo "Please provide the url as parameter"
+    echo ""
+    echo "Usage: ./add-to-cart.sh http://XX.XX.XX.XX/carts/1/items"
+    exit 1
+fi
+
 url=$1
 
 i=0
@@ -9,6 +17,6 @@ while true
 do
   echo ""
   echo "adding item to cart..."
-  curl -X POST -H "Content-Type: application/json" -d "{\"id\":\"3395a43e-2d88-40de-b95f-e00e1502085b\"}" $url
+  curl -X POST -H "Content-Type: application/json" -d "{ \"id\":\"3395a43e-2d88-40de-b95f-e00e1502085b\" }" $url
   i=$((i+1))
 done
